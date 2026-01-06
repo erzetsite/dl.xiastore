2ed8dcd9b87ee95a3b7f5ffbcbde6b38512d9be4
erzetsite <akhyarer7.business@gmail.com>
2026-01-06 20:34:49 +0700

    fix: change cheerio import to star import for cloudflare compatibility

--- functions/services/tiktokService.js
+++ functions/services/tiktokService.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import cheerio from "cheerio";
+import * as cheerio from "cheerio";
 
 async function fetchTikTokData(videoUrl) {
   const endpoint = "https://tikdownloader.io/api/ajaxSearch";
